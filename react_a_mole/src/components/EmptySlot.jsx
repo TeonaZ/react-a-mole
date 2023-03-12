b3864deaa752688794fefa99d0d094d0431bac6e
Teona <teonazaalishvili2@gmail.com>
2023-03-12 11:41:32 -0400

    Create React App

---
+++ react_a_mole/src/components/EmptySlot.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useEffect} from 'react'
+import moleImg from '../assets/molehill.png'
+
+export default function EmptySlot(props) {
+    useEffect(() => {
+        // Feel free to adjust this number as you see fit.
+        // It is your game to tune!
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(true)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+  return (
+    <div>
+      <img style={{'width': '30vw'}}
+    src={moleImg}
+     />   
+    </div>
+  )
+}
